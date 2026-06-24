78b458c6a9d2fff6a0a3d24915aecab7eda88165
tila <tila@10.102.103.245>
2026-06-24 09:41:15 +0530

    first commit

---
+++ devops6/src/main/java/sam/devops6/App.java
@@ -0,0 +1,13 @@
+package sam.devops6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ program6/src/test/java/sam/program6/AppTest.java
@@ -0,0 +1,19 @@
+package sam.program6;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
